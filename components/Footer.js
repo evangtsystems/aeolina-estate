fc7083236d45645b2c705216e4569e7a2bfe88ba
evangtsystems <evangelos.lampos@gtsystems.gr>
2026-01-26 15:10:55 +0200

    /

--- components/Footer.js
+++ components/Footer.js
@@ -27,19 +27,19 @@ export default function Footer() {
           </p>
           <p style={{ marginBottom: '8px' }}>
             Follow us:
-            <a href="https://facebook.com/ionianems" target="_blank" rel="noopener noreferrer" style={{ color: 'white', marginLeft: '10px' }}>
+            <a href="" target="_blank" rel="noopener noreferrer" style={{ color: 'white', marginLeft: '10px' }}>
               <FaFacebook size={20} />
             </a>
-            <a href="https://twitter.com/ionianems" target="_blank" rel="noopener noreferrer" style={{ color: 'white', marginLeft: '10px' }}>
+            <a href="" target="_blank" rel="noopener noreferrer" style={{ color: 'white', marginLeft: '10px' }}>
               <FaTwitter size={20} />
             </a>
-            <a href="https://instagram.com/ionianems" target="_blank" rel="noopener noreferrer" style={{ color: 'white', marginLeft: '10px' }}>
+            <a href="" target="_blank" rel="noopener noreferrer" style={{ color: 'white', marginLeft: '10px' }}>
               <FaInstagram size={20} />
             </a>
-            <a href="https://youtube.com/c/ionianems" target="_blank" rel="noopener noreferrer" style={{ color: 'white', marginLeft: '10px' }}>
+            <a href="" target="_blank" rel="noopener noreferrer" style={{ color: 'white', marginLeft: '10px' }}>
               <FaYoutube size={20} />
             </a>
-            <a href="https://linkedin.com/company/ionianems" target="_blank" rel="noopener noreferrer" style={{ color: 'white', marginLeft: '10px' }}>
+            <a href="" target="_blank" rel="noopener noreferrer" style={{ color: 'white', marginLeft: '10px' }}>
               <FaLinkedin size={20} />
             </a>
           </p>
